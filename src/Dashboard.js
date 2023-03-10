efc9a3cd9ea2f4599016eaecd73f0de024978a9d
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-03-10 15:50:46 +0000

    Remove use of Dashboard.js useEffect() due to potential hook error

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,11 @@
 import "./css/Dashboard.css"
 import EventCards from "./components/EventCards";
 import SearchBar from "./components/SearchBar";
-import { useEffect } from 'react';
 
 export default function Dashboard({ events, setCurrentViewEvent, client, refreshList }) {
 
-	// Refresh the list on component mount
-	useEffect(() => {
-		if (!events) refreshList();
-	}, []);
+	// Get the events list if we don't have it yet
+	if (!events) refreshList();
 
 	return (
 		<>
